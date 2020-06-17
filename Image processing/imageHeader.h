7eac47439dafc6f3a31b5e6774be04b4742931ba
Musa Bah <balamusa.bah@gmail.com>
2020-06-16 20:25:58 -0500

    ignore file added

---
+++ Image processing/imageHeader.h
@@ -0,0 +1,12 @@
+//  imageHeader.h
+//  Image processing
+//
+//  Created by Musa Bah on 6/16/20.
+//  Copyright © 2020 Musa Bah. All rights reserved.
+//
+
+#ifndef imageHeader_h
+#define imageHeader_h
+
+
+#endif /* imageHeader_h */
